b8585bdcb8451723c09c97261bbb6004a2eb9e44
agessaman <adam@gessaman.com>
2026-08-05 19:58:47 -0700

    feat(mqtt): increase built-in MQTT preset count to 31

--- src/helpers/MQTTPresets.h
+++ src/helpers/MQTTPresets.h
@@ -127,7 +127,7 @@ static const char ISRG_ROOT_X1[] PROGMEM =
     "-----END CERTIFICATE-----\n";
 
 // Number of built-in presets
-static const int MQTT_PRESET_COUNT = 29;
+static const int MQTT_PRESET_COUNT = 31;
 
 // Built-in preset definitions (stored in flash)
 static const MQTTPresetDef MQTT_PRESETS[MQTT_PRESET_COUNT] = {
